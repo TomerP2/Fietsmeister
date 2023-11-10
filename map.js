8e0da6787a0f8159745f7bab225e4f6f6ea10fc5
Tomer Peled <tomer.a.peled@gmail.com>
2023-11-10 15:50:01 +0100

    first leaflet map test

---
+++ map.js
@@ -0,0 +1,9 @@
+var map = L.map('map').fitWorld();
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+map.locate({setView: true, maxZoom: 16});
+
