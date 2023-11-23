4551c86cf927b80dd7ca52196f900d9721c73d9e
Tomer Peled <tomer.a.peled@gmail.com>
2023-11-23 12:57:57 +0100

    Added correct icon to blokkage

--- app/static/map.js
+++ app/static/map.js
@@ -1,14 +1,23 @@
 var map = L.map('map').setView([51.6951, 5.333135], 16);
-var wfs_url = 'http://localhost:8181/geoserver/fietsmeister/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fietsmeister%3Ablokkages&outputFormat=application%2Fjson'
+var wfsUrl = 'http://localhost:8181/geoserver/fietsmeister/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fietsmeister%3Ablokkages&outputFormat=application%2Fjson'
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-fetch(wfs_url)
+var BlokkeringIconGroot = L.icon({
+  iconUrl: "/static/blokkering_icon_groot.png",
+  iconSize:     [30, 30], // size of the icon
+  iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+});
+
+fetch(wfsUrl)
   .then(response => response.json())
   .then(data => {
-    var blokkages = L.geoJSON(data).addTo(map);
+    L.geoJSON(data, {
+      pointToLayer: function(feature, latlng) {
+        return L.marker(latlng, { icon: BlokkeringIconGroot });
+      }
+    }).addTo(map)
   })
-
 map.locate({setView: true});
