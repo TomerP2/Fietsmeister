b46a1b25f14d73339ea06bef1e12b7a8830ab4bd
Tomer Peled <tomer.a.peled@gmail.com>
2023-11-23 11:45:32 +0100

    Now fetching GeoJSON from geoserver

--- app/static/map.js
+++ app/static/map.js
@@ -1,13 +1,14 @@
-var map = L.map('map').fitWorld();
-var blokkages_wfs = 'http://localhost:8181/geoserver/fietsmeister/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fietsmeister%3Ablokkages&outputFormat=application%2Fjson'
+var map = L.map('map').setView([51.6951, 5.333135], 16);
+var wfs_url = 'http://localhost:8181/geoserver/fietsmeister/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fietsmeister%3Ablokkages&outputFormat=application%2Fjson'
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-var blokkages = L.tileLayer.wfs(blokkages_wfs, {
-    // Add any additional options here
-  }).addTo(map);
+fetch(wfs_url)
+  .then(response => response.json())
+  .then(data => {
+    var blokkages = L.geoJSON(data).addTo(map);
+  })
 
-map.locate({setView: true, maxZoom: 16});
+map.locate({setView: true});
