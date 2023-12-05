8c0bfa5c55260adf3d00550769a6bbadb9587995
Tomer Peled <tomer.a.peled@gmail.com>
2023-12-05 13:43:08 +0100

    Added API endpoint for marking point true

--- app/map.py
+++ app/map.py
@@ -14,6 +14,7 @@ bp = Blueprint('map', __name__)
 def index():
     return render_template('map/map.html')
 
+
 @bp.route('/api/blokkageinfo/<int:id>', methods=['GET'])
 def get_blokkage_info(id):
     cursor = get_cursor()
@@ -36,3 +37,49 @@ def get_blokkage_info(id):
         "marked_false": int(row[3])
     }
     return jsonify(data)
+
+
+@bp.route('/api/marktrue', methods=['POST'])
+def mark_true():
+    try:
+        # Extract data from the request
+        blokkage_id = request.form.get("blokkage_id")
+        user_id = request.form.get("user_id")
+
+        # Validate input
+        if not blokkage_id or not user_id:
+            raise ValueError("Invalid input. Both blokkage_id and user_id are required.")
+
+        # Convert IDs to integers
+        blokkage_id = int(blokkage_id)
+        user_id = int(user_id)
+
+        # Check if the blokkage exists (optional, depending on your requirements)
+        if not blokkage_exists(blokkage_id):
+            raise ValueError(f"Blokkage with ID {blokkage_id} does not exist.")
+
+        cursor = get_cursor()
+        db = get_db()
+
+        # Use triple double quotes for the SQL query
+        QUERY = "INSERT INTO marked_true (blokkage_id, created_by) VALUES (%s, %s)"
+
+        cursor.execute(QUERY, (blokkage_id, user_id))
+        db.commit()
+
+        return jsonify({"success": True, "message": "Marked as true successfully."})
+
+    except ValueError as ve:
+        return jsonify({"success": False, "error": str(ve)})
+
+    except Exception as e:
+        # Log the exception for debugging purposes
+        print(f"An error occurred: {str(e)}")
+        return jsonify({"success": False, "error": "An unexpected error occurred."})
+
+def blokkage_exists(blokkage_id):
+    cursor = get_cursor()
+    QUERY = "SELECT COUNT(*) FROM blokkages WHERE id = %s"
+    cursor.execute(QUERY, (blokkage_id,))
+    count = cursor.fetchone()[0]
+    return count == 1
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,6 @@
 import json
+from app.db import get_cursor
+import pytest
 
 def test_get_blokkage_info(client):
     response = client.get('/api/blokkageinfo/1')
@@ -8,3 +10,23 @@ def test_get_blokkage_info(client):
     assert data["days_ago"] == 0
     assert data["marked_true"] == 2
     assert data["marked_false"] == 1
+
+
+@pytest.mark.parametrize(('blokkage_id', 'user_id', 'message', 'blokkages_count'), (
+    (1, '', b"Invalid input. Both blokkage_id and user_id are required.", 2), #No user_id
+    ('', 1, b"Invalid input. Both blokkage_id and user_id are required.", 2), # No blokkage_id
+    (200, 1, b"Blokkage with ID 200 does not exist.", 2), #Nonexistent blokkage_id
+    (1, 1, b"Marked as true successfully.", 3) #Valid input
+))
+def test_mark_blokkage_true(client, app, blokkage_id, user_id, message, blokkages_count):
+    response = client.post(
+    '/api/marktrue',
+    data={'blokkage_id': blokkage_id, 'user_id': user_id}
+    )
+
+    assert message in response.data
+    with app.app_context():
+        cursor = get_cursor()
+        cursor.execute("SELECT COUNT(*) FROM marked_true WHERE blokkage_id = 1")
+        count = cursor.fetchone()[0]
+        assert count == blokkages_count
