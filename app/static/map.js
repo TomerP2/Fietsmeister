356251a6762ebcc476d4e331afb3804a533fed11
Tomer Peled <tomer.a.peled@gmail.com>
2023-12-12 11:25:59 +0100

    Bunch of stuff. Sorry bad commit

--- app/static/map.js
+++ app/static/map.js
@@ -1,45 +1,35 @@
 // Set some global variables
-let editModeEnabled = false
+let editModeEnabled = false;
+let blokkagesLayer = null;
+let map = null;
+let userInfo = null;
+const wfsUrl = "http://localhost:8181/geoserver/fietsmeister/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fietsmeister%3Ablokkages&outputFormat=application%2Fjson";
+const basemapUrl = 'https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/pastel/EPSG:3857/{z}/{x}/{y}.png'
+const BlokkeringIconGroot = createBlokkeringIcon();
+const reportButtonElement = document.getElementById('report-button');
+const reportTextElement = document.getElementById('report-text');
+
 
 async function main(){
-  // Get user info
-  let userInfo = await getCurrentUserInfo()
+  map = L.map("map").setView([51.6951, 5.333135], 16);
 
-  // Initialize the map
-  const map = L.map("map").setView([51.6951, 5.333135], 16);
+  userInfo = await getCurrentUserInfo();
 
-  // Add Basemap layer
   getBasemap().addTo(map);
 
-  // Create BlokkeringIconGroot icon
-  const BlokkeringIconGroot = createBlokkeringIcon();
+  reportButtonElement.addEventListener('click', function(){toggleEditMode(true)})
 
-  // Set up event listener for 'report' button
-  const reportButtonElement = document.getElementById('report-button')
-  const reportTextElement = document.getElementById('report-text')
-  reportButtonElement.addEventListener('click', function(){
-    console.log('report button clicked');
-    editModeEnabled = true;
-    reportButtonElement.style.display = 'none';
-    reportTextElement.style.display = 'block'
-  })
-
-  // Set up event listener for map location
   map.locate({ setView: true });
 
-  // Fetch and add GeoJSON data to the map
-  const wfsUrl = "http://localhost:8181/geoserver/fietsmeister/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fietsmeister%3Ablokkages&outputFormat=application%2Fjson";
-  addGeoJSONToMap(map, wfsUrl, BlokkeringIconGroot, userInfo);
+  addOrUpdateBlokkagesLayer();
 
-  // Add click event listener to the map
   map.on("click", function (e) {
-    createBlokkage(e.latlng, userInfo)
+    createBlokkage(e.latlng)
   });
 }
 
-// Function to get the basemap layer
 function getBasemap() {
-  return L.tileLayer('https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/pastel/EPSG:3857/{z}/{x}/{y}.png', {
+  return L.tileLayer(basemapUrl, {
     minZoom: 6,
     maxZoom: 19,
     bounds: [[50.5, 3.25], [54, 7.6]],
@@ -47,7 +37,19 @@ function getBasemap() {
   });
 }
 
-// Function to create the BlokkeringIconGroot icon
+function toggleEditMode(on) {
+  console.log(`report mode toggled: ${on}`);
+  if (on) {
+    editModeEnabled = true;
+    reportButtonElement.style.display = 'none';
+    reportTextElement.style.display = 'block';
+  } else{
+    editModeEnabled = false;
+    reportButtonElement.style.display = 'block';
+    reportTextElement.style.display = 'none';
+  }
+};
+
 function createBlokkeringIcon() {
   return L.icon({
     iconUrl: "/static/blokkering_icon_groot.png",
@@ -56,28 +58,29 @@ function createBlokkeringIcon() {
   });
 }
 
-// Function to fetch and add GeoJSON data to the map
-async function addGeoJSONToMap(map, wfsUrl, icon, userInfo) {
+async function addOrUpdateBlokkagesLayer() {
   try {
     const response = await fetch(wfsUrl);
     const data = await response.json();
 
-    createLayerFromJson(data, icon, map, userInfo).addTo(map);
+    if (blokkagesLayer) {
+      map.removeLayer(blokkagesLayer);
+    }
+
+    blokkagesLayer = createLayerFromJson(data);
+    blokkagesLayer.addTo(map);
   } catch (error) {
     console.error("Error fetching or processing GeoJSON:", error);
   }
 }
 
-// Function to create layer from the GeoJSON and add neccesary stuff to layer
-function createLayerFromJson(data, icon, map, userInfo) {
+function createLayerFromJson(data) {
   return L.geoJSON(data, {
     pointToLayer: function (feature, latlng) {
-      const marker = L.marker(latlng, { icon: icon });
+      const marker = L.marker(latlng, { icon: BlokkeringIconGroot });
       const point_id = feature.id.split('.')[1]
 
-      // Add click events to each marker
       marker.on('click', function () {
-        // Display information about the clicked point
         displayPointInfo(point_id, userInfo, latlng, map);
       });
 
@@ -86,35 +89,58 @@ function createLayerFromJson(data, icon, map, userInfo) {
   });
 }
 
- // Function to create a new blokkage
- function createBlokkage(latlng, userInfo) {
+function createBlokkage(latlng) {
   if (!(editModeEnabled)) {
-    console.log('edit mode not enabled. Wont add a new blokkage')
-    return
+    console.log('edit mode not enabled. Won\'t add a new blokkage');
+    return;
   }
 
-  var url = 'http://127.0.0.1:8080/api/createblokkage';
-  var data = {
-    lat: latlng.lat,
-    lng: latlng.lng,
-    user_id: userInfo.id
-  };
+  var tempMarker = L.marker(latlng, { icon: BlokkeringIconGroot }).addTo(map);
 
-  // Using Fetch API to make a POST request
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log('Server response:', data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  var checkMarkerInterval = setInterval(function () {
+    if (map.hasLayer(tempMarker)) {
+      clearInterval(checkMarkerInterval);
+      showConfirmation();
+    }
+  }, 100);
+
+  function showConfirmation() {
+    var userResponse = window.confirm('Place point here?');
+
+    if (userResponse) {
+      var url = 'http://127.0.0.1:8080/api/createblokkage';
+      var data = {
+        lat: latlng.lat,
+        lng: latlng.lng,
+        user_id: userInfo.id
+      };
+
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Server response:', data);
+        map.removeLayer(tempMarker);
+        toggleEditMode(false);
+        addOrUpdateBlokkagesLayer();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        map.removeLayer(tempMarker);
+        toggleEditMode(false);
+        addOrUpdateBlokkagesLayer();
+      });
+    } else {
+      map.removeLayer(tempMarker);
+      toggleEditMode(false);
+      addOrUpdateBlokkagesLayer();
+    }
+  }
 }
 
 main();
