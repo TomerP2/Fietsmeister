2fa754786feb875bd79803405626e87d9c8d15cc
TomerP2 <tomer.a.peled@gmail.com>
2024-01-10 14:01:30 +0100

    Changed conn_str definition to use get() function

--- config/production.py
+++ config/production.py
@@ -4,7 +4,7 @@ import os
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 # Uncomment the following lines for App Service
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 # DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
