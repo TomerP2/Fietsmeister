3a39bd2216cb01add16879937333aee6d2e00dd1
TomerP2 <tomer.a.peled@gmail.com>
2024-01-11 09:47:14 +0100

    test to fix build error

--- config/production.py
+++ config/production.py
@@ -15,6 +15,8 @@ DATABASE_URI = conn_str
 #     dbname=conn_str_params['Database']
 # )
 
+# test
+
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(';')}
 DATABASE_URI = 'dbname={dbname} user={dbuser} password={dbpass} host={dbhost}'.format(
     dbuser=conn_str_params['User Id'],
