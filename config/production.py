51f94dca75a07005df6618981b6c341dcdfb0a5d
TomerP2 <tomer.a.peled@gmail.com>
2024-01-11 09:15:53 +0100

    changed database_uri string to match the development string

--- config/production.py
+++ config/production.py
@@ -15,6 +15,13 @@ DATABASE_URI = conn_str
 #     dbname=conn_str_params['Database']
 # )
 
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(';')}
+DATABASE_URI = 'dbname={dbname} user={dbuser} password={dbpass} host={dbhost}'.format(
+    dbuser=conn_str_params['User Id'],
+    dbpass=conn_str_params['Password'],
+    dbhost=conn_str_params['Server'],
+    dbname=conn_str_params['Database']
+)
 
 SECRET_KEY=os.environ.get('SECRET_KEY')
 GEOSERVER_URL=os.environ.get('GEOSERVER_URL')
